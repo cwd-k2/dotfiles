49cf18abf8ab4e2d5dd700002b461d9aafac91ad
d-flat-aug7 <kaito.augmented7th@gmail.com>
2019-02-02 13:34:28 +0900

    [zsh] プロンプトのパスの表示を短縮するようにしてみた

--- zsh/rc.d/prompt.zsh
+++ zsh/rc.d/prompt.zsh
@@ -10,23 +10,43 @@ zstyle ':vcs_info:*' actionformats "%K{237}%F{248} %b | %a %F{237}%c%u"
 
 function precmd {
     vcs_info
+    tmp_path_indicator
 }
 
-pr1="%F{237}%K{248} %m  %~ %F{248}"
+pr1="%F{237}%K{248} %m  "
 
-PROMPT='$pr1${vcs_info_msg_0_}%K{000}
+PROMPT='$pr1$pr2${vcs_info_msg_0_}%K{000}
 $pr3 %F{223}'
 
-function zle-keymap-select zle-line-init zle-line-finish {
+function tmp_path_indicator() {
+    if [[ "${PWD}" =~ "${HOME}" ]] ; then
+        local tmp_path="~${PWD#$HOME}"
+    else
+        local tmp_path="${PWD}"
+    fi
+
+    if [[ "${#tmp_path}" -gt 30 ]] ; then
+        local basename="$(basename "${tmp_path}")"
+        local shorten="$(echo "${tmp_path%/$basename}" | tr "/" "\n" | cut -c 1 | tr "\n" "/")"
+        tmp_path="${shorten}${basename}"
+    fi
+
+    pr2="${tmp_path} %F{248}"
+}
+
+function keymap_indicator() {
     case "${KEYMAP}" in
         main|viins)
-            pr3="%K{248}%F{237} %BVIINS%b %F{248}%K{000}"
+            pr3="%K{248}%F{237}%B VIINS %b%F{248}%K{000}"
             ;;
         vicmd)
-            pr3="%K{109}%F{237} %BVICMD%b %F{109}%K{000}"
+            pr3="%K{109}%F{237}%B VICMD %b%F{109}%K{000}"
             ;;
     esac
+}
 
+function zle-keymap-select zle-line-init zle-line-finish {
+    keymap_indicator
     zle reset-prompt
 }
 
