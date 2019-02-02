9f96f8d2b66432bb26ab885258d9b193fa3380d9
d-flat-aug7 <kaito.augmented7th@gmail.com>
2019-02-02 16:11:11 +0900

    ぬ

--- zsh/rc.d/completion.zsh
+++ zsh/rc.d/completion.zsh
@@ -7,7 +7,7 @@ zstyle ':zle:*' word-style unspecified
 zstyle ':completion:*:default' menu select=2
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' ignore-parents parent pwd ..
-zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin
+zstyle ':completion:*:sudo:*' command-path $path
 zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
 
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
--- zsh/rc.d/keybind.zsh
+++ zsh/rc.d/keybind.zsh
@@ -1,4 +1,6 @@
 # keybind settings ---------------------------------------------------
+bindkey -v
+
 bindkey -M viins '^N' down-line-or-history
 bindkey -M viins '^P' up-line-or-history
 bindkey -M viins '^L' vi-cmd-mode
