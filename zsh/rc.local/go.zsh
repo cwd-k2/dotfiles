bb862742b6ca2d61c28b367953a7653fcae4afdf
d-flat-aug7 <kaito.augmented7th@gmail.com>
2019-02-04 22:29:29 +0900

    [zsh] GOPATH 間違えてた

--- zsh/rc.local/go.zsh
+++ zsh/rc.local/go.zsh
@@ -1,4 +1,4 @@
-export GOPATH="${EXTRA_APPS_HOME}/go:${HOME/Workspace}/go"
+export GOPATH="${EXTRA_APPS_HOME}/go:${HOME}/Workspace/go"
 export GOBIN="${EXTRA_APPS_HOME}/go/bin"
 
 prepend_path "${GOBIN}"
