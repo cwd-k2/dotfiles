1c156ff7ca3e78ff3af8d003b06f3a192cdc6fc8
d-flat-aug7 <kaito.augmented7th@gmail.com>
2019-05-13 07:29:59 +0900

    [zsh] command functions for RoR

--- zsh/rc.local/ruby.zsh
+++ zsh/rc.local/ruby.zsh
@@ -4,3 +4,19 @@ export GEM_SPEC_CACHE="${XDG_CACHE_HOME}/gem/specs"
 export BUNDLE_USER_CONFIG="${XDG_CONFIG_HOME}/bundle"
 export BUNDLE_USER_PLUGIN="${XDG_DATA_HOME}/bundle"
 export BUNDLE_USER_CACHE="${XDG_CACHE_HOME}/bundle"
+
+function bundle() {
+    if [[ "${1}" = "install" ]] ; then
+        command bundle "${@}" --path vendor/bundle
+    else
+        command bundle "${@}"
+    fi
+}
+
+function rails() {
+    if [[ "${1}" = "new" ]] ; then
+        command bundle exec rails "${@}" --skip-bundle
+    else
+        command bundle exec rails "${@}"
+    fi
+}
