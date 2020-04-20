f0193d7e3898836efefb366468fc9f96c7701de6
cwd-k2 <kaito.augmented7th@gmail.com>
2020-04-20 09:45:37 +0900

    enhancd remove

--- zsh/rc.local/zplugin.zsh
+++ zsh/rc.local/zplugin.zsh
@@ -3,11 +3,6 @@ if [[ -d "${EXTRA_APPS_HOME}/zplugin" ]] ; then
     export ZPLGM[HOME_DIR]="${EXTRA_APPS_HOME}/zplugin"
     . "${ZPLGM[HOME_DIR]}/bin/zplugin.zsh"
 
-    export ENHANCD_FILTER="fzf --reverse --border --cycle --height=10"
-    export ENHANCD_DIR="${XDG_CACHE_HOME}/enhancd"
-    export ENHANCD_COMMAND="ef"
-
-    zplugin light "b4b4r07/enhancd"
     zplugin light "zsh-users/zsh-autosuggestions"
 
     autoload -Uz _zplugin
