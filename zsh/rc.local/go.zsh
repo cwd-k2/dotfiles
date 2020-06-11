50a70ac7b8a7587c635a0d7283f895d7885c0566
cwd-k2 <kaito.augmented7th@gmail.com>
2020-06-11 22:47:55 +0900

    [zsh] trancate GOPATH

--- zsh/rc.local/go.zsh
+++ zsh/rc.local/go.zsh
@@ -1,4 +1,4 @@
-export GOPATH="${EXTRA_APPS_HOME}/go:${HOME}/Workspace/OpenProjects/go:${HOME}/Workspace/PrivateProjects/go"
+export GOPATH="${EXTRA_APPS_HOME}/go"
 export GOBIN="${EXTRA_APPS_HOME}/go/bin"
 
 prepend_path "${GOBIN}"
