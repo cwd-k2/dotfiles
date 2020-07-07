0626e5d1b55b3177678e750f07fe2e65c5654578
cwd-k2 <kaito.augmented7th@gmail.com>
2020-07-07 23:51:36 +0900

    [zsh] just a small touch

--- zsh/rc.local/node.zsh
+++ zsh/rc.local/node.zsh
@@ -1,3 +1,4 @@
 export NODE_REPL_HISTORY="${XDG_CACHE_HOME}/node/repl_history"
+
 export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
 export NPM_CONFIG_CACHE="${XDG_CACHE_HOME}/npm"
--- zsh/rc.local/zplugin.zsh
+++ zsh/rc.local/zplugin.zsh
@@ -3,10 +3,9 @@ if [[ -d "${EXTRA_APPS_HOME}/zplugin" ]] ; then
     export ZPLGM[HOME_DIR]="${EXTRA_APPS_HOME}/zplugin"
     . "${ZPLGM[HOME_DIR]}/bin/zplugin.zsh"
 
-    zplugin light "zsh-users/zsh-autosuggestions"
-
     autoload -Uz _zplugin
     (( ${+_comps} )) && _comps[zplugin]=_zplugin
 
+    zplugin light "zsh-users/zsh-autosuggestions"
     zplugin light "zdharma/fast-syntax-highlighting"
 fi
