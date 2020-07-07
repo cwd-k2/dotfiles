df41e7f0eb3903953db4812d1723e4c8f13a4564
cwd-k2 <kaito.augmented7th@gmail.com>
2020-07-07 18:33:06 +0900

    [zsh] 色々

--- zsh/rc.local/colorls.zsh
+++
@@ -1,9 +0,0 @@
-if [[ "$(which colorls > /dev/null ; echo $?)" -eq 0 ]] ; then
-    alias k='colorls'
-    alias ka='colorls -A'
-    alias kk='colorls -l'
-    alias kka='colorls -Al'
-    alias t='colorls --tree'
-    alias ta='colorls --tree -A'
-fi
-
