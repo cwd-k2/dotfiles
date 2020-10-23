f6ec3eef5abb2539daf41afdf4858305b9fd4ae4
cwd-k2 <kaito.augmented7th@gmail.com>
2020-10-23 09:12:47 +0900

    [zsh] haskell => set STACK_ROOT

--- zsh/rc.local/haskell.zsh
+++ zsh/rc.local/haskell.zsh
@@ -1,2 +1,4 @@
-export CABAL_DIR=${EXTRA_APPS_HOME}/cabal
-prepend_path ${CABAL_DIR}/bin
+export CABAL_DIR="${EXTRA_APPS_HOME}/cabal"
+export STACK_ROOT="${EXTRA_APPS_HOME}/stack"
+
+prepend_path "${CABAL_DIR}/bin"
