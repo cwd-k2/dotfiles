037d1df51f5b524dc06894e25cd5459f347b1d6a
cwd-k2 <kaito.augmented7th@gmail.com>
2020-10-24 19:26:33 +0900

    [zsh] alias => bat --sort Name (case sensitive)

--- zsh/rc.d/alias.zsh
+++ zsh/rc.d/alias.zsh
@@ -2,7 +2,7 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 
-which exa > /dev/null && alias ls='exa --color=always --icons'
+which exa > /dev/null && alias ls='exa --color=always --icons --sort=Name'
 which bat > /dev/null && alias cat='bat'
 
 alias la='ls -a'
