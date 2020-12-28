ff68add0a105123973a4a7b25ac458cb3cb6e931
cwd-k2 <kaito.augmented7th@gmail.com>
2020-12-28 19:33:24 +0900

    [zsh] fzf => use system package manager

--- zsh/rc.local/fzf.zsh
+++ zsh/rc.local/fzf.zsh
@@ -1,6 +1,14 @@
-if [[ -d "${EXTRA_APPS_HOME}/fzf" ]] ; then
-    . "${EXTRA_APPS_HOME}/fzf/shell/completion.zsh"
-    . "${EXTRA_APPS_HOME}/fzf/shell/key-bindings.zsh"
-
-    prepend_path "${EXTRA_APPS_HOME}/fzf/bin"
-fi
+case "${OSTYPE}" in
+  darwin*)
+    if [[ -d "/usr/local/opt/fzf" ]] ; then
+      . "/usr/local/opt/fzf/shell/completion.zsh"
+      . "/usr/local/opt/fzf/shell/key-bindings.zsh"
+    fi
+    ;;
+  linux*)
+    if [[ -d "/usr/share/fzf" ]] ; then
+      . "/usr/share/fzf/completion.zsh"
+      . "/usr/share/fzf/key-bindings.zsh"
+    fi
+    ;;
+esac
