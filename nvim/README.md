7bb54fc8cc60e4af715f0a79f90f621504e70ca6
cwd-k2 <29105991+cwd-k2@users.noreply.github.com>
2024-06-17 07:20:16 +0900

    [zsh] update prompt theme using omz within zinit

--- nvim/README.md
+++ nvim/README.md
@@ -4,8 +4,6 @@ my neovim configuration files.
 
 ## dependencies
 
-- node
-
 ```sh
 $ go get -u golang.org/x/tools/cmd/goimports
 $ go get -u golang.org/x/tools/gopls
@@ -15,12 +13,3 @@ $ go get -u golang.org/x/tools/gopls
 
 - Use `compile_flags.txt`
 - Set `Allow clipboard access to terminal apps` for iTerm2 (macOS)
-- For swift
-  1. clone `github.com/apple/swift`
-  1. move `utils/vim/{ftplugin,ftdetect,syntax}` to `$XDG_DATA_HOME/nvim/site`
-
-## directory hierarchy
-
-under `rc.d`, non-plugin-related (neovim unique) rc-files are located.
-
-in `pc.d`, there are plugin-related configuration files.
--- zsh/env.d/zplugin.zsh
+++ zsh/env.d/zplugin.zsh
@@ -1,11 +1,22 @@
-if [[ -d "${EXTRA_APPS_HOME}/zplugin" ]] ; then
-    typeset -gAH ZPLGM
-    export ZPLGM[HOME_DIR]="${EXTRA_APPS_HOME}/zplugin"
-    . "${ZPLGM[HOME_DIR]}/bin/zinit.zsh"
+typeset -gAH ZPLGM
+export ZPLGM[HOME_DIR]="${EXTRA_APPS_HOME}/zplugin"
 
-    autoload -Uz _zinit
-    (( ${+_comps} )) && _comps[zplugin]=_zinit
+if [[ ! -d ${ZPLGM[HOME_DIR]} ]] ; then
+  git clone https://github.com/zdharma-continuum/zinit.git ${ZPLGM[HOME_DIR]}
+fi
 
-    zinit light "zsh-users/zsh-autosuggestions"
-    zinit light "zdharma/fast-syntax-highlighting"
+. "${ZPLGM[HOME_DIR]}/zinit.zsh"
+
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zplugin]=_zinit
+
+zinit light "zsh-users/zsh-autosuggestions"
+zinit light "zdharma/fast-syntax-highlighting"
+
+zinit wait    lucid for \
+  OMZL::git.zsh atload"unalias grv" OMZP::git
+zinit wait'!' lucid for \
+  OMZL::async_prompt.zsh OMZL::prompt_info_functions.zsh OMZT::candy
+
+if [[ -n "${SSH_CLIENT}" ]] ; then
 fi
